04f4b83d0f2cef416b9560f0b5c3fcdbe10fd5e0
sylvain joly <sylvain.joly00@orange.fr>
2020-03-11 18:09:24 +0100

    deploy heroku

--- index.js
+++ index.js
@@ -3,10 +3,15 @@
 
 const express = require('express');
 const app = express();
+const axios = require('axios');
 
 
 app.get('/', (req,res,next) => {
 res.status(200).json({msg: "salut app 1"});
+});
+
+app.get('/history', (req, res, next) => {
+    res.status(200).json({history: "todo"})
 });
 
 app.listen(3000, () => {
